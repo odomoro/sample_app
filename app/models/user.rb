b8b2262daad873d54937b6f38f38bc91f1a51d44
unknown <Odone@Odone-PC.(none)>
2013-01-03 17:06:47 -0200

    Validacao de email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,16 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
   
   before_save { |user| user.email = email.downcase }
   
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,24 +2,30 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
 #
 
 require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
-
+  before do
+     @user = User.new( name: "Example User", email: "user@example.com", 
+                       password: "foobar", password_confirmation: "foobar")
+  end
+  
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
-  
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
   it { should be_valid }
 
   describe "when name is not present" do
@@ -64,6 +70,21 @@ describe User do
       user_with_same_email.save
     end
 
+    it { should_not be_valid }
+  end
+  
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
+  
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+  
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
     it { should_not be_valid }
   end
   

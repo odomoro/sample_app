dee39763e10aecd97ad1aa411a6cf787e0172bc6
Odone M da Rosa <odone@intelidata.inf.br>
2013-01-03 23:22:14 -0200

    Terminando a configuracao de password e todos os testes green

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,11 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
   
   before_save { |user| user.email = email.downcase }
-  
+  validates :password, presence: true, length: {minimum: 6}
+  validates :password_confirmation, presence: true
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},  uniqueness: { case_sensitive: false }

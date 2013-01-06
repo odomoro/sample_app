734b081202a02e6a09fd91b10b1004c48e041664
unknown <Odone@Odone-PC.(none)>
2013-01-05 23:06:45 -0200

    aparentemente final do cap8

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,29 @@
 module SessionsHelper
+  
+  #grava o cookie quando salva o login
+  def sign_in(user)
+    cookies.permanent[:remember_token] = user.remember_token
+    self.current_user = user
+  end 
+  
+  #apaga o cookie para forçar o signout
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
+  end
+  
+  #metodo usado para verificar se o usuario esta ou nao logado
+  def signed_in?
+    !current_user.nil?
+  end
+  
+  #atribui o usuario para a variavel de instancia @current_user
+  def current_user=(user)
+    @current_user = user
+  end
+  
+  #procura o usuario com base no cookie, caso a variavel de instância @current_user esteja = nil
+  def current_user
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
+#  remember_token  :string(255)
 #
 
 class User < ActiveRecord::Base
@@ -15,9 +16,18 @@ class User < ActiveRecord::Base
   has_secure_password
   
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
+ 
   validates :password, presence: true, length: {minimum: 6}
   validates :password_confirmation, presence: true
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},  uniqueness: { case_sensitive: false }
+  
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64      
+    end  
 end
+
+
